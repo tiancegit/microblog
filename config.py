a9459e89ed647b534d4fcb82a63e66ff5456938e
lee <tiance.1984@gmail>
2016-09-05 01:49:10 +0800

    add note

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'TIANCE'
 
-OPENIS_PROVVIDERS = [
+OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
