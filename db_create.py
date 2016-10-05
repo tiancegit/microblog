862c618ffff60e920fd939d1041d0e85649c5757
lee <tiance.1984@gmail.com>
2016-10-05 19:41:14 +0800

    修改了错误

--- db_create.py
+++ db_create.py
@@ -7,14 +7,13 @@
 
 import os.path
 from migrate.versioning import api
-
 from app import db
-from config import SQLALCHEMY_DATABASE_URL
+from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-    api.version_control(SQLALCHEMY_DATABASE_URL, SQLALCHEMY_MIGRATE_REPO)
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
-    api.version_control(SQLALCHEMY_DATABASE_URL, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
