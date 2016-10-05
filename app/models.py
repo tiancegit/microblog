c7cc26577a9076e27c28052c78ba99ded850cab5
lee <tiance.1984@gmail.com>
2016-10-05 19:26:16 +0800

    add db

--- app/models.py
+++ app/models.py
@@ -6,10 +6,9 @@ from app import db
 '''定义数据库模型'''
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
