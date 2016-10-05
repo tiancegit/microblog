0e7b7975b7dff42b68997d06eb86604ef38cd057
lee <tiance.1984@gmail.com>
2016-10-05 19:23:32 +0800

    add db

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repositoryr')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 CSRF_ENABLED = True
 SECRET_KEY = 'TIANCE'
 
